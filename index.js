a650fd9d63b8026e6d36ba5935ff3b48d90213e9
andyjohn23 <andrewnick906@gmail.com>
2022-09-01 13:47:44 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (a, b) => {
+    return a / b;
+}
+divide(2000, 100);
+
+const square = (number) => {
+    return Math.pow(number, 2)
+}
+square(10)
+
+const add = (a, b) => {
+    return a + b;
+}
+add(2, 2);
